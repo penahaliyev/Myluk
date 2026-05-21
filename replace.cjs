0435f128f35e609bf8a233647099f743bbb3b33a
penahaliyev <penahaliyev@gmail.com>
2026-05-21 11:07:27 +0400

    delete21051107

--- replace.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('server.ts', 'utf8');
-code = code.replace(/JSON\.parse\(response\.text\)/g, 'parseGeminiResponse(response.text)');
-fs.writeFileSync('server.ts', code);
